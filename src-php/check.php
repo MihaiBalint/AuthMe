df716938be089b33be6c93b58f46bca08e4b104c
Mihai Balint <nailuii@gmail.com>
2011-04-29 10:30:38 +0300

    fixed the hashing bug

--- src-php/check.php
+++ src-php/check.php
@@ -11,7 +11,7 @@ $thedata = mysql_query($dbquery) or die(mysql_error());
 $numofusers = mysql_num_rows($thedata);
 $row = mysql_fetch_assoc($thedata);
 
-if(md5($row[$user_password_field]+$mainsalt+$salt)==$_GET["pass"]){
+if(md5($row[$user_password_field] . $mainsalt . $salt)==$_GET["pass"]){
 	echo "yes\n<br/>";
 }
 else {
