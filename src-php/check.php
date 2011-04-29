26878001ee3a4aa58ba0f009b05b7f2c08cf8216
Mihai Balint <nailuii@gmail.com>
2011-04-29 11:32:34 +0300

    fix lowercase

--- src-php/check.php
+++ src-php/check.php
@@ -2,7 +2,7 @@
 
 require_once 'setting.php';
 require_once 'connect.php';
-$user = $_GET["pn"];
+$user = strtolower($_GET["pn"]);
 $passw = $_GET["sh"];
 $salt = $_GET["si"];
 $dbquery = "SELECT " . "*" ." FROM " . $user_table. " WHERE " . $user_name_field . "=" ."'" .$user."'" ;
